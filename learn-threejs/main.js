e02326f1ab48a3946f209dc309334a666e8b2f44
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-10-02 15:42:20 +0530

    chore(threejs): add comments for better understanding

--- learn-threejs/main.js
+++ learn-threejs/main.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
+// Scene, Camera
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -11,35 +12,41 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 5;
 scene.add(camera);
 
+// Cube - BoxGeometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+// Renderer
 const canvas = document.querySelector("canvas");
-
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 renderer.render(scene, camera);
 
+// Controls - for camera movement
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.dampingFactor = 1;
+controls.dampingFactor = 0.01; // Inertia of the camera movement and lower the value, the slower the camera movement
 controls.enableDamping = true;
+controls.autoRotate = true;
 
 const clock = new THREE.Clock(); // Clock is used to get the time elapsed since the page loaded
 
+// Animation Loop
 function animate() {
   requestAnimationFrame(animate);
+  controls.update();
 
-  cube.rotation.x = clock.getElapsedTime();
-  cube.rotation.y = clock.getElapsedTime();
+  // cube.rotation.x = clock.getElapsedTime();
+  // cube.rotation.y = clock.getElapsedTime();
 
   renderer.render(scene, camera);
 }
 
 animate();
 
+// Resize Event - for responsive design
 window.addEventListener("resize", () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
