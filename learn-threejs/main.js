34da643fdb0e1e1642408045c443d3f0912ec827
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-10-02 15:36:10 +0530

    chore(threejs): add controls and clock with animation

--- learn-threejs/main.js
+++ learn-threejs/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -22,11 +23,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 renderer.render(scene, camera);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.dampingFactor = 1;
+controls.enableDamping = true;
+
+const clock = new THREE.Clock(); // Clock is used to get the time elapsed since the page loaded
+
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  cube.rotation.x = clock.getElapsedTime();
+  cube.rotation.y = clock.getElapsedTime();
 
   renderer.render(scene, camera);
 }
